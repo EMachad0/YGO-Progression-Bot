fc3510d96fb4eac523eb1c4a454af43557f50f0a
EMachad0 <elitonmachadod200@gmail.com>
2021-06-30 23:11:35 -0300

    better db

--- notebooks/db.py
+++ notebooks/db.py
@@ -2,46 +2,40 @@ import os
 import psycopg2
 import psycopg2.extras
 
+conn = None
+
 def connect(verbose=False):
+    global conn
+    if conn is not None:
+        return conn
     try:
         if verbose:
             print('DB Connecting')
-        con = psycopg2.connect(os.environ['DATABASE_URL'])
+        conn = psycopg2.connect(os.environ['DATABASE_URL'])
         if verbose:
             print('DB Connected')
-        return con
+        return conn
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
         raise (Exception('Error while connecting to DB: ' + str(error)))
 
 def make_query(query, data):
-    con = None
-    cur = None
     try:
-        con = connect()
+        con = connect(verbose=True)
         cur = con.cursor()
         cur.execute(query, data)
         con.commit()
+        cur.close()
     except (Exception, psycopg2.Error) as err:
         print(err)
         raise err
-    finally:
-        if con:
-            cur.close()
-            con.close()
-            
+
 def make_select(select, data):
-    con = None
-    cur = None
     try:
-        con = connect()
+        con = connect(verbose=True)
         cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         cur.execute(select, data)
         return cur.fetchall()
     except (Exception, psycopg2.Error) as err:
         print(err)
         raise err
-    finally:
-        if con:
-            cur.close()
-            con.close()
