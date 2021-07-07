5064b747a4524be95ef42b982239cf5f1ce79a70
EMachad0 <elitonmachadod200@gmail.com>
2021-07-06 23:48:47 -0300

    give pack opening refactor

--- extensions/openings.py
+++ extensions/openings.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 from notebooks import db
 
-PLAYER_SELECT = "select player_cod from player p join discord_user du on du.user_cod = p.user_cod where server_cod=%s"
+PLAYER_SELECT = "select player_cod, user_cod from player where server_cod=%s"
 SET_SELECT = "select set_cod from set;"
 INSERT_OPENING = "insert into opening values (x) " \
                  "on conflict (set_cod, player_cod) do " \
@@ -20,13 +20,12 @@ class UserData(commands.Cog):
     async def on_message(self, message):
         user = message.author
         guild = message.guild
-        params = message.content.split() + [None] * 5
+        params = message.content.split()
         if guild is None or message.author == self.client.user:
             return
         if user.guild_permissions.administrator:
             if message.content.startswith('$give_pack'):
                 set_cod, quantity = params[1:3]
-                who = [(" ".join(i.split("#")[:-1]), i.split("#")[-1]) for i in params[3:] if i is not None]
                 if set_cod is None:
                     await message.channel.send("No set_cod! ex = LOB")
                     return
@@ -36,19 +35,20 @@ class UserData(commands.Cog):
                 if quantity is None:
                     await message.channel.send("No quantity! ex = 36")
                     return
-                values = [guild.id]
-                player_select = PLAYER_SELECT
-                if len(who) > 0:
-                    values += sum(who, ())
-                    player_select += " and (" + " or ".join(f"(name=%s and discriminator=%s)" for _ in who) + ");"
-                # print(values, '\n', player_select)
-                rows = db.make_select(player_select, values)        
-                if len(rows) == 0:
+
+                players = db.make_select(PLAYER_SELECT, [guild.id])
+                players = {p["user_cod"]: p["player_cod"] for p in players}
+                if len(params) > 3:
+                    ids = [int(w[3:-1]) for w in params[3:] if w is not None]
+                    players = [players[user_id] for user_id in ids if user_id in players]
+                else:
+                    players = [players[k] for k in players]
+                if len(players) == 0:
                     await message.channel.send("No player found!")
                     return
-                values = sum(([set_cod, p['player_cod'], quantity] for p in rows), []) + [quantity]
-                insert_opening = INSERT_OPENING.replace("(x)", ", ".join(["(default, %s, %s, %s)"] * len(rows)))
-                # print(values, '\n', insert_opening)
+
+                values = sum(([set_cod, p, quantity] for p in players), []) + [quantity]
+                insert_opening = INSERT_OPENING.replace("(x)", ", ".join(["(default, %s, %s, %s)"] * len(players)))
                 db.make_query(insert_opening, values)
                 await message.add_reaction('✅')
                 
