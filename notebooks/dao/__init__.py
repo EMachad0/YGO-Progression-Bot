cf3add14a757e02d08bc2feb1ef9f343d0b572fc
EMachad0 <elitonmachadod200@gmail.com>
2021-07-14 23:04:35 -0300

    more dao

--- notebooks/dao/__init__.py
+++ notebooks/dao/__init__.py
@@ -4,3 +4,5 @@ from notebooks.dao.collection_dao import Collection
 from notebooks.dao.discord_server_dao import DiscordServer
 from notebooks.dao.discord_user_dao import DiscordUser
 from notebooks.dao.pull_dao import Pull
+from notebooks.dao.set_dao import Set
+from notebooks.dao.set_type_dao import SetType
