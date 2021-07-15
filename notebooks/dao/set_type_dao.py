95f2ff2eb2922a999eaaba0f67d87954952e0262
EMachad0 <elitonmachadod200@gmail.com>
2021-07-14 22:04:40 -0300

    list

--- notebooks/dao/set_type_dao.py
+++ notebooks/dao/set_type_dao.py
@@ -5,4 +5,4 @@ class SetType(db.Model):
     __tablename__ = 'set_type'
     set_type_cod = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     num_cards = db.Column(db.Integer)
-    release_date = db.Column(db.Text)
+    list = db.Column(db.Text)
